4dcc67b76c48e58cad177acacc7c514f70b1f025
WinstonMendoza1985 <winston.mendoza1985@gmail.com>
2020-10-20 15:26:27 +0100

    update
    comment

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 	
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css">
-	
+	<link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/theme.css">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
 	</head>
 	<body>
